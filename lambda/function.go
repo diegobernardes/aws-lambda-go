610c5542a9c90a1e458ffc956bd5515224b40f19
Diego Pinto <pintdieg@amazon.com>
2019-12-10 01:42:39 +0000

    wip

--- lambda/function.go
+++ lambda/function.go
@@ -28,51 +28,51 @@ func (fn *Function) Ping(req *messages.PingRequest, response *messages.PingRespo
 	return nil
 }
 
-func (fn Function) Invoke(req messages.InvokeRequest, response messages.InvokeResponse) error {
-	defer func() {
-		if err := recover(); err != nil {
-			panicInfo := getPanicInfo(err)
-			response.Error = &messages.InvokeResponse_Error{
-				Message:    panicInfo.Message,
-				Type:       getErrorType(err),
-				StackTrace: panicInfo.StackTrace,
-				ShouldExit: true,
-			}
-		}
-	}()
+// func (fn Function) Invoke(req messages.InvokeRequest, response messages.InvokeResponse) error {
+// 	defer func() {
+// 		if err := recover(); err != nil {
+// 			panicInfo := getPanicInfo(err)
+// 			response.Error = &messages.InvokeResponse_Error{
+// 				Message:    panicInfo.Message,
+// 				Type:       getErrorType(err),
+// 				StackTrace: panicInfo.StackTrace,
+// 				ShouldExit: true,
+// 			}
+// 		}
+// 	}()
 
-	deadline := time.Unix(req.Deadline.Seconds, req.Deadline.Nanos).UTC()
-	invokeContext, cancel := context.WithDeadline(context.Background(), deadline)
-	defer cancel()
+// 	deadline := time.Unix(req.Deadline.Seconds, req.Deadline.Nanos).UTC()
+// 	invokeContext, cancel := context.WithDeadline(context.Background(), deadline)
+// 	defer cancel()
 
-	lc := &lambdacontext.LambdaContext{
-		AwsRequestID:       req.RequestId,
-		InvokedFunctionArn: req.InvokedFunctionArn,
-		Identity: lambdacontext.CognitoIdentity{
-			CognitoIdentityID:     req.CognitoIdentityId,
-			CognitoIdentityPoolID: req.CognitoIdentityPoolId,
-		},
-	}
-	if len(req.ClientContext) > 0 {
-		if err := json.Unmarshal(req.ClientContext, &lc.ClientContext); err != nil {
-			response.Error = lambdaErrorResponse(err)
-			return nil
-		}
-	}
-	invokeContext = lambdacontext.NewContext(invokeContext, lc)
+// 	lc := &lambdacontext.LambdaContext{
+// 		AwsRequestID:       req.RequestId,
+// 		InvokedFunctionArn: req.InvokedFunctionArn,
+// 		Identity: lambdacontext.CognitoIdentity{
+// 			CognitoIdentityID:     req.CognitoIdentityId,
+// 			CognitoIdentityPoolID: req.CognitoIdentityPoolId,
+// 		},
+// 	}
+// 	if len(req.ClientContext) > 0 {
+// 		if err := json.Unmarshal(req.ClientContext, &lc.ClientContext); err != nil {
+// 			response.Error = lambdaErrorResponse(err)
+// 			return nil
+// 		}
+// 	}
+// 	invokeContext = lambdacontext.NewContext(invokeContext, lc)
 
-	invokeContext = context.WithValue(invokeContext, "x-amzn-trace-id", req.XAmznTraceId)
+// 	invokeContext = context.WithValue(invokeContext, "x-amzn-trace-id", req.XAmznTraceId)
 
-	payload, err := fn.handler.Invoke(invokeContext, req.Payload)
-	if err != nil {
-		response.Error = lambdaErrorResponse(err)
-		return nil
-	}
-	response.Payload = payload
-	return nil
-}
+// 	payload, err := fn.handler.Invoke(invokeContext, req.Payload)
+// 	if err != nil {
+// 		response.Error = lambdaErrorResponse(err)
+// 		return nil
+// 	}
+// 	response.Payload = payload
+// 	return nil
+// }
 
-func (fn *Function) InvokeOld(req *messages.InvokeRequest, response *messages.InvokeResponse) error {
+func (fn *Function) Invoke(req *messages.InvokeRequest, response *messages.InvokeResponse) error {
 	defer func() {
 		if err := recover(); err != nil {
 			panicInfo := getPanicInfo(err)
